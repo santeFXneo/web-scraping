c727e1a1809123fd942565905eab28bfc4b71c62
zihengwang <ziheng.wang@servian.com>
2023-02-11 21:39:27 +1100

    test

---
+++ afl.py
@@ -0,0 +1,26 @@
+from newspaper import Article
+
+url = 'http://fox13now.com/2013/12/30/new-year-new-laws-obamacare-pot-guns-and-drones/'
+article = Article(url)
+
+article.download()
+
+article.html
+
+article.parse()
+
+article.authors
+
+
+article.publish_date
+
+
+article.text
+
+
+article.top_image
+
+
+article.movies
+
+
